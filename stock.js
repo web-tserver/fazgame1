2dc1b2074c5d3b118ae5729d60d4bf7a44b2d9c3
web-tserver <graphinkvisualcreative@gmail.com>
2026-02-03 11:25:24 +0800

    Add files via upload

--- stock.js
+++ stock.js
@@ -1,17 +1,71 @@
-// FAZGAME STOCK DATA
+// stock.js
+// ===============================
+// FAZGAME STOCK (FROM PHOTO)
+// ===============================
+
 export const STOCK = [
-  {
-    n: 'PlayStation 5 Disc',
-    c: 'console',
-    type: 'console',
-    p: 2399,
-    cond: 'new'
-  },
-  {
-    n: 'RTX 3060',
-    c: 'pc',
-    type: 'pc',
-    p: 1299,
-    cond: 'used'
-  }
+
+/* ===============================
+   🎮 PS5 GAMES
+================================ */
+{ n:'Black Myth: Wukong', c:'ps5', type:'game', p:180, cond:'used' },
+{ n:'Marvel Spider-Man 2', c:'ps5', type:'game', p:130, cond:'used' },
+
+// RM100 EACH
+{ n:'Ghost of Tsushima (PS5)', c:'ps5', type:'game', p:100, cond:'used' },
+{ n:'Naruto Storm Connections', c:'ps5', type:'game', p:100, cond:'used' },
+
+{ n:'Hades', c:'ps5', type:'game', p:120, cond:'used' },
+
+// RM80 EACH
+{ n:'Grand Theft Auto V (PS5)', c:'ps5', type:'game', p:80, cond:'used' },
+
+{ n:'Applov: End of God', c:'ps5', type:'game', p:120, cond:'used' },
+
+{ n:'FIFA 22 (PS5)', c:'ps5', type:'game', p:20, cond:'used' },
+{ n:'EA FC 24 (PS5)', c:'ps5', type:'game', p:50, cond:'used' },
+{ n:'FIFA 23 (PS5)', c:'ps5', type:'game', p:30, cond:'used' },
+{ n:'NBA 2K21 (PS5)', c:'ps5', type:'game', p:20, cond:'used' },
+
+/* ===============================
+   🎮 PS4 GAMES
+================================ */
+{ n:'WWE 2K24', c:'ps4', type:'game', p:80, cond:'used' },
+{ n:'Red Dead Redemption 2', c:'ps4', type:'game', p:70, cond:'used' },
+{ n:'Need for Speed Heat', c:'ps4', type:'game', p:60, cond:'used' },
+
+// RM50 EACH
+{ n:'God of War + Horizon Zero Dawn (Mega Pack)', c:'ps4', type:'game', p:50, cond:'used' },
+
+// RM30 EACH
+{ n:'Batman Arkham Knight', c:'ps4', type:'game', p:30, cond:'used' },
+{ n:'God of War (PS4)', c:'ps4', type:'game', p:30, cond:'used' },
+
+// RM50 EACH
+{ n:'Dragon Ball Xenoverse 2', c:'ps4', type:'game', p:50, cond:'used' },
+{ n:'Grand Theft Auto V (PS4)', c:'ps4', type:'game', p:50, cond:'used' },
+{ n:'Monster Hunter World', c:'ps4', type:'game', p:50, cond:'used' },
+
+{ n:'Need for Speed Rivals', c:'ps4', type:'game', p:50, cond:'used' },
+{ n:'Assassin’s Creed Origins', c:'ps4', type:'game', p:50, cond:'used' },
+
+{ n:'FIFA 23 (PS4)', c:'ps4', type:'game', p:50, cond:'used' },
+{ n:'FIFA 22 (PS4)', c:'ps4', type:'game', p:40, cond:'used' },
+{ n:'FIFA 21 (PS4)', c:'ps4', type:'game', p:30, cond:'used' },
+
+{ n:'FIFA 20 (Disc Only)', c:'ps4', type:'game', p:10, cond:'used' },
+{ n:'PES 2020', c:'ps4', type:'game', p:30, cond:'used' },
+{ n:'PES 2019', c:'ps4', type:'game', p:20, cond:'used' },
+
+// RM10 EACH
+{ n:'Mafia III: Definitive Edition', c:'ps4', type:'game', p:10, cond:'used' },
+{ n:'Destiny: The Taken King', c:'ps4', type:'game', p:10, cond:'used' },
+
+{ n:'Anthem', c:'ps4', type:'game', p:10, cond:'used' },
+
+// RM20 EACH
+{ n:'Monster Hunter World (Alt)', c:'ps4', type:'game', p:20, cond:'used' },
+{ n:'Dragon Quest Builders', c:'ps4', type:'game', p:20, cond:'used' }
+
 ];
+
